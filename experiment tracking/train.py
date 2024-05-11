d8929e27b0d0dba1947185a259f3045bddfe1008
Senura Hansaja <ifire810@gmail.com>
2024-05-11 22:54:24 +0700

    tracking

--- experiment tracking/train.py
+++ experiment tracking/train.py
@@ -1,3 +1,17 @@
 import matplotlib.pyplot as plt 
 import torch
 import torchvision
+
+from torch import nn
+from torchvision import transforms
+
+## try to get the torch info installed to the code
+try :
+    import torchinfo
+except:
+    print("Couldn't find the specific package...")
+
+
+## importing the previous data and function calls
+try :
+    from modular Development import data_setup, engine
